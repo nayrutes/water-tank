38ff83088b420cb7584448dc5bfc1b8b5be82779
LAPTOP-FELIX\Felix <->
2024-06-26 20:06:58 +0200

    fixed some export issues

--- MainMenu.gd
+++ MainMenu.gd
@@ -5,11 +5,15 @@ extends MarginContainer
 @onready var main: Container = $main
 @onready var credits: Container = $credits
 @onready var settings: Container = $settings
+@onready var lvlsDisplay: Label = $main/VBoxContainer/ButtonStart/LvlsDisplay
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_button.grab_focus()
 	main.visibility_changed.connect(_on_vis_change)
+	var txt: String = "Levels: " + str(GameLoop.level_count)
+	lvlsDisplay.text = txt
+	print("Main menu ready")
 	pass # Replace with function body.
 
 func _on_vis_change():
